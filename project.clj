4db4a815e1a0c4c92bbb74b8d05ee08389d465dc
Jesse Reiss <jessereiss@squareup.com>
2014-03-26 12:25:56 -0700

    Update licenses and readme. Add seesaw dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject bugz "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :description "A Fun Clojure Game With Battling Bugs"
+  :url "http://https://github.com/thegorgon/bugz"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.4"]]
   :main ^:skip-aot bugz.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
