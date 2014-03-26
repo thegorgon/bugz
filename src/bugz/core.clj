e27a504f04ac4ec0fb0c0581a46575a86b45af5b
David Faber <dafaber@gmail.com>
2014-03-26 15:32:13 -0700

    Make neighborhoods

--- src/bugz/core.clj
+++ src/bugz/core.clj
@@ -2,11 +2,34 @@
   (:gen-class)
   (:use seesaw.core))
 
-(defn -main [& args]
-  (invoke-later
-    (-> (frame :title "Hello",
-            :content "Hellow, Seesaw",
-            :on-close :exit)
-    pack!
-    show!)))
+
+(defn build-world [] (atom {}))
+
+
+
+;; {:sugar-spots
+;;  [{:location [x y] :qty 100}]
+;;  :trails
+;;  :colonies
+;;  [
+;;   {:ants []
+;;    :sugar 0
+;;    :holes []}]}
+
+(def x (ref 10))
+(def y (ref 0))
+
+(defn build-frame []
+  (-> (frame :title "howdy"
+             :width 400
+             :height 400)))
+
+(defn show-frame [f]
+  (show! f))
+
+
+
+
+
+(defn -main [& args])
 
--- src/bugz/world.clj
+++ src/bugz/world.clj
@@ -1,7 +1,14 @@
 (ns bugz.world)
 
 (defn empty [width height]
-  (vec (repeat height (vec (repeat width (ref {}))))))
+  (vec (repeatedly height
+                   (fn [] (vec (repeatedly width (fn [] (ref {}))))))))
 
 (defn tile-at [world x y]
   ((world y) x))
+
+(defn neighborhood [world x y r]
+  (for [dx (range (- r) r) dy (range (- r) r)
+        :let [x' (+ x dx) y' (+ y dy)]
+        :when (and (not (neg? x')) (not (neg? y')))]
+    [[dx dy] (tile-at world x' y')]))
